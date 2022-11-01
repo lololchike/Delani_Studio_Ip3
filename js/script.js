1b9220652071a2bfa93dc64b98e448b12086926a
Lucy Ipato Ololchike <lucyololchike@gmail.com>
2022-11-01 20:52:39 +0300

    file

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 
-    $(document).ready(function () {
-        $("#btn1").click(function () {
-            $("#design").show();
-        });
-        $("#btn2").click(function () {
-            $("#service").show();
-        });
-        $("#btn3").click(function () {
-            $("#product").show();
-        });
+$(document).ready(function () {
+    $("#btn1").hover(function () {
+        $("#design").show();
+    });
+    $("#btn2").hover(function () {
+        $("#service").show();
+    });
+    $("#btn3").hover(function () {
+        $("#product").show();
+    });
+
         
     });
+
 
