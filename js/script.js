8dc657c3c2fac94f1b3ab7e4c34bf4162cf67768
Lucy Ipato Ololchike <lucyololchike@gmail.com>
2022-11-03 16:16:47 +0300

    file update

--- js/script.js
+++ js/script.js
@@ -33,7 +33,35 @@ $(document).ready(function () {
       $(".i-product").toggle();
       $(".i-product").show();
     });
-     
+});
+    $(document).ready(function () {
+      $(".text").hover(
+        function () {
+          $(this).animate({ opacity: "1" });
+        },
+        function () {
+          $(this).animate({ opacity: "0" });
+        }
+      );
+    });
+
+
+    $(document).ready(function () {
+      $("#form").submit(function (event) {
+        var userName = $("input#NAME").val();
+        var email = $("input#EMAIL").val();
+        if (userName && email != "") {
+          alert(
+            "Thank you " +
+              userName +
+              "." +
+              " we have received your message,Thank you for reaching out to us."
+          );
+        } else {
+          alert("Please enter your name and email");
+        }
+        event.preventDefault();
+      });
     });
 
 
