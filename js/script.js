f508eb4e5190b07b92349194c757ba4927905db1
Lucy Ipato Ololchike <lucyololchike@gmail.com>
2022-11-03 19:24:36 +0300

    file

--- js/script.js
+++ js/script.js
@@ -36,41 +36,90 @@ $(document).ready(function () {
 
 
 
-     $(".portfolio").mouseenter("hover", function () {
-    $(".portfolio").show();
-       $("#h1").toggle();
-       $("#h1").show();
+     $(".work1").mouseenter("hover", function () {
+       $(".work1").show();
+       $(".project1").toggle();
+       $(".project1").show();
      });
-     $("#h1").mouseenter("hover", function () {
-       $("#h1").hide();
-       $(".portfolio").toggle();
-       $(".portfolio").show();
+     $(".project1").mouseenter("hover", function () {
+       $(".project1").hide();
+       $(".work1").toggle();
+       $(".work1").show();
      });
-
-
+    
+$(".work2").mouseenter("hover", function () {
+  $(".work2").show();
+  $(".project2").toggle();
+  $(".project2").show();
 });
-    $(document).ready(function () {
-      $(".text").hover(
-        function () {
-          $(this).animate({ opacity: "1" });
-        },
-        function () {
-          $(this).animate({ opacity: "0" });
-          });
-        
-
-        
-         });
-  $(document).ready(function(){
-      $('.text').hover(function () {
-      $(this).animate({opacity:'1'});
-      },
-      function () {
-      $(this).animate({opacity:'0'});
-  })
+$(".project2").mouseenter("hover", function () {
+  $(".project2").hide();
+  $(".work2").toggle();
+  $(".work2").show();
+});
+    $(".work3").mouseenter("hover", function () {
+      $(".work3").show();
+      $(".project3").toggle();
+      $(".project3").show();
+    });
+    $(".project3").mouseenter("hover", function () {
+      $(".project3").hide();
+      $(".work3").toggle();
+      $(".work3").show();
+    });
+    $(".work4").mouseenter("hover", function () {
+      $(".work4").show();
+      $(".project4").toggle();
+      $(".project4").show();
+    });
+    $(".project4").mouseenter("hover", function () {
+      $(".project4").hide();
+      $(".work4").toggle();
+      $(".work4").show();
+    });
+    $(".work5").mouseenter("hover", function () {
+      $(".work5").show();
+      $(".project5").toggle();
+      $(".project5").show();
+    });
+    $(".project5").mouseenter("hover", function () {
+      $(".project5").hide();
+      $(".work5").toggle();
+      $(".work5").show();
+    });
+    $(".work6").mouseenter("hover", function () {
+      $(".work6").show();
+      $(".project6").toggle();
+      $(".project6").show();
+    });
+    $(".project6").mouseenter("hover", function () {
+      $(".project6").hide();
+      $(".work6").toggle();
+      $(".work6").show();
+    });
+    $(".work7").mouseenter("hover", function () {
+      $(".work7").show();
+      $(".project7").toggle();
+      $(".project7").show();
+    });
+    $(".project7").mouseenter("hover", function () {
+      $(".project7").hide();
+      $(".work7").toggle();
+      $(".work7").show();
+    });
+    $(".work8").mouseenter("hover", function () {
+      $(".work8").show();
+      $(".project8").toggle();
+      $(".project8").show();
+    });
+    $(".project8").mouseenter("hover", function () {
+      $(".project8").hide();
+      $(".work8").toggle();
+      $(".work8").show();
     });
 
-
+});
+ 
     $(document).ready(function () {
       $("#form").submit(function (event) {
         var userName = $("input#NAME").val();
